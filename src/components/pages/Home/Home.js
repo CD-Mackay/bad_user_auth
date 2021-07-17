b32ff3a50f20f9eb56c7b039ddd5379bb18fd8a1
Connor Mackay <connor.mackay@gmail.com>
2021-07-17 17:53:45 -0400

    add password confirmation field

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -12,10 +12,22 @@ const Home = () => {
 
   const [name, setName] = useState("")
   const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
+  const [phase, setPhase] = useState(1);
   return (
     <StyledHome>
+      
+      {phase === 1 && 
+      <div className="form-field">
       <TextInput type={"username"} fieldValue={name} updateValue={setName} labelMessage={"Please enter your username"} />
       <TextInput type={"password"} fieldValue={password} updateValue={setPassword} labelMessage={"Please enter your password"} />
+      </div>}
+      {phase === 2 && 
+      <div className="form-field">
+      <TextInput type={"confirmPassword"} fieldValue={confirmPassword} updateValue={setConfirmPassword} labelMessage={"Please confirm your password by entering it in reverse!"} />
+      </div>
+      }
+      <button onClick={() => setPhase(phase + 1)}>Sign up!</button>
     </StyledHome>
   )
 };
